4888912aa6dd2ae2e1640c0bf3ad5c3af885cabe
manishankarR <manishankarraavi2003@gmail.com>
2026-09-06 16:07:34 +0530

    Fix server host for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const postRoutes = require("./routes/postRoutes");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
-const app =  express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +16,9 @@ app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
+app.get("/", (req, res) => {
+    res.send("3W Social App Backend is running!");
+});
 
 mongoose
     .connect(process.env.MONGO_URI)
@@ -24,14 +27,10 @@ mongoose
 
         const PORT = process.env.PORT || 5000;
 
-        app.listen(PORT, () => {
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`Server running on port ${PORT}`);
         });
     })
     .catch((error) => {
         console.error("MongoDB connection failed:", error.message);
     });
-
-app.get("/", (req, res) => {
-    res.send("3W Social App Backend is running!");
-});
